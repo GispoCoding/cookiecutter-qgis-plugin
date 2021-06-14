4186257b0a68a3439adb3c614580c2df012820fc
Joonalai <joona@gispo.fi>
2021-06-14 16:04:42 +0300

    Fix test

--- {{cookiecutter.project_directory}}/test/test_plugin.py
+++ {{cookiecutter.project_directory}}/test/test_plugin.py
@@ -1,4 +1,4 @@
 from {{cookiecutter.plugin_package}}.qgis_plugin_tools.tools.resources import plugin_name
 
 def test_plugin_name():
-    assert plugin_name() == "{{cookiecutter.plugin_name}}"
+    assert plugin_name() == "{{cookiecutter.plugin_name|replace(' ', '')}}"
