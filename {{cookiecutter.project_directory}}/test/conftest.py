1e2a830659ddd509b50e55d1e20aff61b4794948
Joonalai <33314057+Joonalai@users.noreply.github.com>
2021-08-03 12:59:18 +0300

    Add pytest-qgis to requirements-dev.txt and remove duplicate fixtures (#26)

--- {{cookiecutter.project_directory}}/test/conftest.py
+++ {{cookiecutter.project_directory}}/test/conftest.py
@@ -2,47 +2,14 @@
 # flake8: noqa ANN201
 """
 This class contains fixtures and common helper function to keep the test files shorter
+
+pytest-qgis (https://pypi.org/project/pytest-qgis) contains the following helpful fixtures:
+
+* qgis_app initializes and returns fully configured QgsApplication.
+  This fixture is called automatically on the start of pytest session.
+* qgis_canvas initializes and returns QgsMapCanvas
+* qgis_iface returns mocked QgsInterface
+* new_project makes sure that all the map layers and configurations are removed.
+  This should be used with tests that add stuff to QgsProject.
+
 """
-from typing import Tuple
-
-import pytest
-from PyQt5.QtWidgets import QWidget
-from qgis.core import QgsApplication
-from qgis.gui import QgsMapCanvas
-
-from {{cookiecutter.plugin_package}}.qgis_plugin_tools.testing.qgis_interface import QgisInterface
-from {{cookiecutter.plugin_package}}.qgis_plugin_tools.testing.utilities import get_qgis_app
-
-
-@pytest.fixture(autouse=True, scope="session")
-def initialize_qgis() -> Tuple[QgsApplication, QgsMapCanvas, QgisInterface, QWidget]:
-    """Initializes qgis session for all tests"""
-    yield get_qgis_app()
-
-
-@pytest.fixture(scope="session")
-def qgis_app(initialize_qgis) -> QgsApplication:
-    return initialize_qgis[0]
-
-
-@pytest.fixture(scope="session")
-def canvas(initialize_qgis) -> QgsMapCanvas:
-    return initialize_qgis[1]
-
-
-@pytest.fixture(scope="session")
-def iface(initialize_qgis) -> QgisInterface:
-    return initialize_qgis[2]
-
-
-@pytest.fixture(scope="session")
-def qgis_parent(initialize_qgis) -> QWidget:
-    return initialize_qgis[3]
-
-
-@pytest.fixture(scope="function")
-def new_project(iface) -> None:
-    """
-    Initializes new QGIS project by removing layers and relations etc.
-    """
-    yield iface.newProject()
