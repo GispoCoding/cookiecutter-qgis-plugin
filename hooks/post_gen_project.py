01d29ab1a0c7709b3d1a01c3345aef7eb29863e7
Lauri Kajan <lauri.kajan@gispo.fi>
2023-04-01 16:09:59 +0300

    Supress hook outputs on std

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,39 @@
+import logging
 import os
 import shutil
 import subprocess
 import sys
+from typing import List
+
+logger = logging.getLogger(__name__)
 
 ALL_TEMP_FOLDERS = ("licenses", "plugin_templates")
 QGIS_PLUGIN_TOOLS_SPECIFIC_FILES = (
     "{{cookiecutter.plugin_package}}/build.py",
     "test/test_plugin.py",
 )
+
+
+class Colors:
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+
+
+def warn(message: str) -> None:
+    print(f"{Colors.WARNING}Warning: {message}{Colors.ENDC}")
+
+
+def _run(args: List[str]) -> None:
+    try:
+        logger.info(f'Running command "{" ".join(args)}"')
+        subprocess.run(
+            args,
+            capture_output=True,
+            check=True,
+        )
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        warn(f'Running command "{" ".join(args)}" failed.')
 
 
 def _remove_dir(dirpath):
@@ -21,11 +47,11 @@ def _remove_file(filepath):
 
 
 def git_init():
-    subprocess.call(["git", "init"])
+    _run(["git", "init"])
 
 
 def add_plugin_tools():
-    subprocess.call(
+    _run(
         [
             "git",
             "submodule",
@@ -42,20 +68,30 @@ def remove_plugin_tools():
 
 
 def add_remote():
-    subprocess.call(["git", "remote", "add", "origin", "{{cookiecutter.git_repo_url}}"])
+    _run(["git", "remote", "add", "origin", "{{cookiecutter.git_repo_url}}"])
 
 
 def write_dependencies():
-    subprocess.call(
-        [
-            sys.executable,
-            "-m",
-            "piptools",
-            "compile",
-            "--upgrade",
-            "requirements-dev.in",
-        ]
-    )
+    try:
+        subprocess.run(
+            [
+                sys.executable,
+                "-m",
+                "piptools",
+                "compile",
+                "--upgrade",
+                "-o",
+                "requirements-dev.txt",
+                "requirements-dev.in",
+            ],
+            capture_output=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError:
+        warn(
+            "Updating dependecies failed. Do you have the pip-tools installed? "
+            'Run "pip-compile requirements-dev.in" manually in your plugin folder.'
+        )
 
 
 def remove_temp_folders():
